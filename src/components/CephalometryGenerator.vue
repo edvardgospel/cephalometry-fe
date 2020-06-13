b52c941ee78d3339068a8fc7a48b425c85eb8ccf
Edvard Eros <edvard.eros@yahoo.com>
2020-06-13 13:08:14 +0300

    Implemented file upload and reset

--- src/components/CephalometryGenerator.vue
+++ src/components/CephalometryGenerator.vue
@@ -2,9 +2,24 @@
   <div class="cephalometry-div">
     <div class="inner-cephalometry-div">
       <div class="content-div">
-        <div class="photo-div">
+        <div class="photo-div" :class="{'photo-div-active': cephalometricImage.length > 0}">
+          <div class="image-div" v-show="cephalometricImage.length > 0">
+            <img
+              class="image-img"
+              id="image-img-id"
+              :src="cephalometricImage"
+              @click="imageClicked()"
+              ref="imageHtml"
+            />
+          </div>
           <div class="file-div">
-            <input type="file" id="file" class="file-upload-input" />
+            <input
+              class="file-upload-input"
+              type="file"
+              id="file"
+              @change="previewImage"
+              accept="image/*"
+            />
             <div class="label-div">
               <label for="file">Choose a file</label>
             </div>
@@ -12,11 +27,12 @@
         </div>
         <div class="elements-div">
           <div class="labels-div">
-            <span class="cephalometric-point-span">N</span>
-            <span class="reset-span">Reset</span>
+            <span class="cephalometric-point-span">{{cephalometricPoints[index]}}</span>
+            <span class="reset-points-span" @click="index = 0">Reset points</span>
+            <span class="reset-image-span" @click="removeImage">Reset image</span>
           </div>
           <div class="button-div">
-            <button class="generate-button">Generate</button>
+            <button v-show="cephalometricPoints.length === index" class="generate-button">Generate</button>
           </div>
         </div>
       </div>
@@ -26,7 +42,56 @@
 
 <script>
 export default {
-  name: "CephalometryGenerator"
+  name: "CephalometryGenerator",
+  data() {
+    return {
+      cephalometricImage: "", // we will store base64 format of image in this string
+      cephalometricPoints: [
+        "N",
+        "S",
+        "Ba",
+        "Sp",
+        "A",
+        "Pm",
+        "is",
+        "isa",
+        "B",
+        "Pg",
+        "Gn",
+        "ii",
+        "iia",
+        "Ar",
+        "Sp'",
+        "tgo",
+        "PG",
+        "N'",
+        "UL"
+      ],
+      index: 0
+    };
+  },
+  methods: {
+    previewImage(event) {
+      // Reference to the DOM input element
+      var input = event.target;
+      // Ensure that you have a file before attempting to read it
+      if (input.files && input.files[0]) {
+        // create a new FileReader to read this image and convert to base64 format
+        var reader = new FileReader();
+        // Define a callback function to run, when FileReader finishes its job
+        reader.onload = e => {
+          // Note: arrow function used here, so that "this.imageData" refers to the imageData of Vue component
+          // Read image as base64 and set to imageData
+          this.cephalometricImage = e.target.result;
+        };
+        // Start the reader job - read file as a data url (base64 format)
+        reader.readAsDataURL(input.files[0]);
+      }
+    },
+    removeImage() {
+      this.cephalometricImage = "";
+    }
+  }
 };
 </script>
 
@@ -59,6 +124,25 @@ export default {
   margin: 0 auto;
   overflow: hidden;
   border: 1px solid #dddddd;
+}
+
+.photo-div-active {
+  border: none;
+}
+
+.image-div {
+  position: relative;
+  height: 100%;
+  width: 100%;
+}
+
+.image-img {
+  position: relative;
+  width: 100%;
+}
+
+.image-img:hover {
+  cursor: crosshair; /* "hand" cursor */
 }
 
 .file-div {
@@ -110,7 +194,17 @@ label:hover {
   cursor: default;
 }
 
-.reset-span {
+.reset-points-span {
+  border: 1px solid #dddddd;
+  border-radius: 5px;
+  padding-right: 5px;
+  padding-left: 5px;
+  margin-left: 5px;
+  margin-right: 5px;
+  cursor: pointer; /* "hand" cursor */
+}
+
+.reset-image-span {
   border: 1px solid #dddddd;
   border-radius: 5px;
   padding-right: 5px;

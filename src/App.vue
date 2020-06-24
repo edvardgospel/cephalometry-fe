28cf40d7078b4858d6711525cb56c96b7162d2d9
Edvard Eros <edvard.eros@yahoo.com>
2020-06-24 15:55:35 +0300

    Created LoadingScreen.vue and ErrorScreen.vue

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,8 @@
           v-else-if="currentComponent === 'menu'"
           :cephalometryResponse="cephalometryResponse"
         />
+        <LoadingScreen v-else-if="currentComponent === 'loading'" />
+        <ErrorScreen v-else-if="currentComponent === 'error'" />
       </div>
     </div>
     <Footer author="Edvard Eros" email="edvard.eros@yahoo.com" />
@@ -26,6 +28,8 @@ import NavigationBar from "./components/NavigationBar.vue";
 import CephalometryForm from "./components/CephalometryForm.vue";
 import CephalometryGenerator from "./components/CephalometryGenerator.vue";
 import CephalometryMenu from "./components/CephalometryMenu.vue";
+import LoadingScreen from "./components/LoadingScreen.vue";
+import ErrorScreen from "./components/ErrorScreen.vue";
 import Footer from "./components/Footer.vue";
 import api from "./Api.js";
 
@@ -36,6 +40,8 @@ export default {
     CephalometryForm,
     CephalometryGenerator,
     CephalometryMenu,
+    LoadingScreen,
+    ErrorScreen,
     Footer
   },
   data() {
@@ -55,17 +61,19 @@ export default {
     addCephalometryCoordinates(cephalometryCoordinates) {
       this.cephalometryCoordinates = cephalometryCoordinates;
       this.createRequest();
+      this.currentComponent = "loading";
       api
         .createNew(this.cephalometryRequest)
         .then(response => {
           this.cephalometryResponse = response.data;
+          this.currentComponent = "menu";
         })
         .catch(error => {
+          //TODO change this
           console.log(error);
+          this.currentComponent = "error";
         })
-        .finally(() => {
-          this.currentComponent = "menu";
-        });
+        .finally(() => {});
     },
     createRequest() {
       this.cephalometryRequest.personalData = this.personalData;

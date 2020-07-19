ddf33bcbdb4b0506f7091b02bb61f26f595a5b03
Edvard Eros <edvard.eros@yahoo.com>
2020-07-19 19:28:05 +0300

    Harmony Table looks good, not perfect

--- src/components/cephalometry/menu/HarmonyTable.vue
+++ src/components/cephalometry/menu/HarmonyTable.vue
@@ -9,12 +9,14 @@
       <div class="table-header-div">ML-NL</div>
     </div>
     <div class="inner-harmony-table-values-div">
+      <canvas id="canvas" width="640" height="600"></canvas>
+      <div class="extra-space-div"></div>
       <div class="table-values-div">
         <div v-for="el in sna" :key="el.id" :class="{red: el==snaAngle}">{{el}}</div>
       </div>
       <div class="table-values-div">
         <div
-          style="padding: 5px"
+          style="padding-bottom: 10.6px"
           v-for="el in nl_nsl"
           :key="el.id"
           :class="{red: el==nlNslAngle}"
@@ -22,7 +24,7 @@
       </div>
       <div class="table-values-div">
         <div
-          style="padding: 11.5px"
+          style="padding-bottom: 23.5px"
           v-for="el in nsba"
           :key="el.id"
           :class="{red: el==nsBaAngle}"
@@ -30,23 +32,29 @@
       </div>
       <div class="table-values-div">
         <div
-          style="padding: 5px"
+          style="padding-bottom: 8.9px"
           v-for="el in ml_nsl"
           :key="el.id"
           :class="{red: el==mlNslAngle}"
         >{{el}}</div>
       </div>
       <div class="table-values-div">
-        <div style="padding: 3px" v-for="el in snb" :key="el.id" :class="{red: el==snbAngle}">{{el}}</div>
+        <div
+          style="padding-bottom: 5.5px"
+          v-for="el in snb"
+          :key="el.id"
+          :class="{red: el==snbAngle}"
+        >{{el}}</div>
       </div>
       <div class="table-values-div">
         <div
-          style="padding: 4px"
+          style="padding-bottom: 6.5px"
           v-for="el in ml_nl"
           :key="el.id"
           :class="{red: el==mlNlAngle}"
         >{{el}}</div>
       </div>
+      <div style="float:left" class="extra-space-div"></div>
     </div>
   </div>
 </template>
@@ -58,9 +66,9 @@ export default {
   name: "HarmonyTable",
   data() {
     return {
-      sna: Array(103 - 61 + 1)
+      sna: Array(103 - 60 + 1)
         .fill()
-        .map((_, i) => 61 + i),
+        .map((_, i) => 60 + i),
       nl_nsl: [
         "15",
         "·",
@@ -93,18 +101,19 @@ export default {
         "1",
         "·"
       ],
-      nsba: Array(142 - 122 + 1)
+      nsba: Array(142 - 121 + 1)
         .fill()
-        .map((_, i) => 122 + i)
+        .map((_, i) => 121 + i)
         .reverse(),
-      ml_nsl: Array(43 - 14 + 1)
+      ml_nsl: Array(44 - 13 + 1)
         .fill()
-        .map((_, i) => 14 + i)
+        .map((_, i) => 13 + i)
         .reverse(),
-      snb: Array(97 - 64 + 1)
+      snb: Array(97 - 63 + 1)
         .fill()
-        .map((_, i) => 64 + i),
+        .map((_, i) => 63 + i),
       ml_nl: [
+        "29",
         "·",
         "28",
         "·",
@@ -147,70 +156,73 @@ export default {
       );
     },
     snaAngle() {
-      console.log(
-        "snaAngle:" +
-          this.cephalometricAngles
-            .find(angle => angle.name === "SNA")
-            .angle.toFixed()
-      );
       return this.cephalometricAngles
         .find(angle => angle.name === "SNA")
         .angle.toFixed();
     },
     nlNslAngle() {
-      console.log(
-        "nlNslAngle:" +
-          this.cephalometricAngles
-            .find(angle => angle.name === "NL-NSL")
-            .angle.toFixed()
-      );
       return this.cephalometricAngles
         .find(angle => angle.name === "NL-NSL")
         .angle.toFixed();
     },
     nsBaAngle() {
-      console.log(
-        "nsBaAngle:" +
-          this.cephalometricAngles
-            .find(angle => angle.name === "NSBa")
-            .angle.toFixed()
-      );
       return this.cephalometricAngles
         .find(angle => angle.name === "NSBa")
         .angle.toFixed();
     },
     mlNslAngle() {
-      console.log(
-        "mlNslAngle:" +
-          this.cephalometricAngles
-            .find(angle => angle.name === "ML-NSL")
-            .angle.toFixed()
-      );
       return this.cephalometricAngles
         .find(angle => angle.name === "ML-NSL")
         .angle.toFixed();
     },
     snbAngle() {
-      console.log(
-        "snbAngle:" +
-          this.cephalometricAngles
-            .find(angle => angle.name === "SNB")
-            .angle.toFixed()
-      );
       return this.cephalometricAngles
         .find(angle => angle.name === "SNB")
         .angle.toFixed();
     },
     mlNlAngle() {
-      console.log(
-        "mlNlAngle:" +
-          this.cephalometricAngles
-            .find(angle => angle.name === "ML-NL")
-            .angle.toFixed()
-      );
       return this.cephalometricAngles
         .find(angle => angle.name === "ML-NL")
         .angle.toFixed();
+    }
+  },
+  mounted() {
+    var canvas = document.getElementById("canvas");
+    if (canvas.getContext) {
+      var context = canvas.getContext("2d");
+      context.beginPath();
+      context.moveTo(0, 273);
+      context.lineTo(106, 273);
+      context.lineTo(106, 153);
+      context.lineTo(212, 153);
+      context.lineTo(212, 146);
+      context.lineTo(320, 146);
+      context.lineTo(320, 193);
+      context.lineTo(426, 193);
+      context.lineTo(426, 273);
+      context.lineTo(640, 273);
+
+      context.moveTo(0, 362);
+      context.lineTo(106, 362);
+      context.lineTo(106, 460);
+      context.lineTo(212, 460);
+      context.lineTo(212, 478);
+      context.lineTo(320, 478);
+      context.lineTo(320, 441);
+      context.lineTo(426, 441);
+      context.lineTo(426, 362);
+      context.lineTo(640, 362);
+
+      context.lineWidth = "0.7";
+      context.strokeStyle = "#2c3e50";
+      context.stroke();
+
+      context = canvas.getContext("2d");
+      context.beginPath();
+      context.setLineDash([15, 10]); /*dashes are 5px and spaces are 3px*/
+      context.moveTo(0, 320);
+      context.lineTo(640, 320);
+      context.stroke();
     }
   }
 };
@@ -230,7 +242,19 @@ export default {
 
 .inner-harmony-table-values-div {
   height: 94%;
+  width: 100%;
   overflow: scroll;
+}
+
+/* Hide scrollbar for Chrome, Safari and Opera */
+.inner-harmony-table-values-div::-webkit-scrollbar {
+  display: none;
+}
+
+/* Hide scrollbar for IE, Edge and Firefox */
+.inner-harmony-table-values-div {
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
 }
 
 .table-header-div,
@@ -240,11 +264,22 @@ export default {
   width: 16.66%;
 }
 
+.extra-space-div {
+  height: 25%;
+  width: 100%;
+}
+
 .table-header-div {
   margin: 0.6rem auto;
 }
 
 .red {
   color: red;
+}
+
+canvas {
+  display: block;
+  position: absolute;
+  z-index: -1;
 }
 </style>
